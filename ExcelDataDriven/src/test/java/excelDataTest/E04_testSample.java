80c559091f5ae99603267c6456466127ead5adc6
sadia01 <sadiabintenizam@gmail.com>
2024-08-12 02:54:34 +0600

    Upload Download From Excel External

---
+++ ExcelDataDriven/src/test/java/excelDataTest/E04_testSample.java
@@ -0,0 +1,20 @@
+package excelDataTest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class E04_testSample {
+
+	public static void main(String[] args) throws IOException {
+		
+		E03_dataDrivenExcel d = new E03_dataDrivenExcel();
+		ArrayList data = d.getData("Add Profile");
+				
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+
+	}
+
+}
---
+++ ExcelDataDriven/src/test/java/excelDataTest/T02_dataProviderTestNG.java
@@ -0,0 +1,38 @@
+package excelDataTest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class T02_dataProviderTestNG {
+	
+	@Test
+	public void getExcel() throws IOException
+	{
+		FileInputStream fis=new FileInputStream((System.getProperty("user.dir")+"//src//test//java//excelData//dataProvider.xlsx"));
+		XSSFWorkbook workbook=new XSSFWorkbook(fis);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		XSSFRow row = sheet.getRow(0);
+		int colCount = row.getLastCellNum();
+		
+		Object data[][] = new Object[rowCount-1][colCount];
+		
+		for(int i=0; i<rowCount-1;i++)
+		{
+			
+			row = sheet.getRow(i+1);
+			
+			for(int j=0; j<colCount;j++)
+			{
+				data[i][j] = row.getCell(j);
+			}
+			
+		}
+	}
+
+}
